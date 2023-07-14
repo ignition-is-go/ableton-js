e7625d9b31f130eaff12c72d45d2f24653a89454
DM Welch <dmw9400@gmail.com.com>
2023-07-14 18:28:19 -0400

    <feat> update to match my local working .py files

--- midi-script/Chain.py
+++ midi-script/Chain.py
@@ -1,6 +1,5 @@
 from __future__ import absolute_import
 from .Interface import Interface
-from .Device import Device
 from .MixerDevice import MixerDevice
 
 
@@ -15,12 +14,23 @@ class Chain (Interface):
             "id": chain_id,
             "name": chain.name
         }
-    
+    @staticmethod
+    def serialize_device(device):
+        if device is None:
+            return None
+
+        device_id = Interface.save_obj(device)
+        return {
+            "id": device_id,
+            "name": device.name,
+            "type": str(device.type),
+            "class_name": device.class_name,
+        }
     def __init__(self, c_instance, socket):
-      super(Chain, self).__init__(c_instance, socket)
+     super(Chain, self).__init__(c_instance, socket)
 
     def get_devices(self, ns):
-        return map(Device.serialize_device, ns.devices)
+        return map(Chain.serialize_device, ns.devices)
     
     def get_mixer_device(self, ns):
-        return map(MixerDevice.serialize_MixerDevice, ns.mixer_device)
+        return map(MixerDevice.serialize_mixer_device, ns.mixer_device)
--- midi-script/Device.py
+++ midi-script/Device.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 from .Interface import Interface
 from .DeviceParameter import DeviceParameter
 from .Chain import Chain
-from .DrumPad import DrumPad
+
 
 class Device(Interface):
     @staticmethod
@@ -23,22 +23,9 @@ class Device(Interface):
 
     def get_parameters(self, ns):
         return map(DeviceParameter.serialize_device_parameter, ns.parameters)
-    
-    def get_chains(self, ns):
-        return map(Chain.serialize_chain, ns.chains)
-        
-    def get_return_chains(self, ns): 
-        return map(Chain.serialize_chain, ns.return_chains )
 
     def get_type(self, ns):
         return str(ns.type)
     
-    def get_class_name(self, ns):
-        return str(ns.class_name)
-    
-    def get_name(self, ns):
-        return str(ns.name)
-        
-    def get_drum_pads(self, ns):
-        return map(DrumPad.serialize_drum_pad, ns.drum_pads)
-    
+    def get_chains(self, ns):
+        return map(Chain.serialize_chain, ns.chains)
